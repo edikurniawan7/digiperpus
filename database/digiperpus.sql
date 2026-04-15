07e673222939d7177d96557ae98fe5b660f3544a
edikurniawan7 <edikrn577@gmail.com>
2026-04-15 12:41:14 +0700

    final final

--- database/digiperpus.sql
+++ database/digiperpus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 11 Apr 2026 pada 06.53
+-- Waktu pembuatan: 15 Apr 2026 pada 07.40
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `digiperpus-app`
+-- Database: `digiperpus`
 --
 
 -- --------------------------------------------------------
@@ -56,9 +56,10 @@ INSERT INTO `buku` (`id_buku`, `id_kategori`, `judul`, `pengarang`, `penerbit`, 
 (21, 3, 'Dompet Ayah Sepatu Ibu', 'JS Khairen', 'Gramedia Widiasarana Indonesia', '2023', 31, '1775618320_sepatu.avif', 'Zenna lahir urutan keenam dari sebelas saudara. Ia bersama keluarganya tinggal di punggung gunung Singgalang. Saat kecil, Zenna sudah bekerja keras untuk hidup. Ia pergi ke sekolah dengan sepatu rombeng naik-turun gunung sambil membawa jagung rebus untuk dijual. “Besok Abak belikan sepatu baru kalau sudah dapat uang,” janji Abaknya pada Zenna sebelum berangkat ke sekolah. Namun tak sempat Abak tunaikan janji itu. Abak meninggalkan Zenna untuk selamanya, juga meninggalkan janjinya pada Zenna untuk membelikan sepatu. Sebagai anak tengah-tengah, Zenna jarang mendapat perhatian. Ia menumpahkan kesedihannya pada dirinya sendiri. Ia bekerja keras dengan mandiri. Ia ingin melanjutkan janji Abaknya untuk membelikan sepatu. Ia membeli sepatu untuk dirinya sendiri. Di punggung gunung yang lain, gunung Marapi, Asrul dan adiknya Irsal harus membantu Umi untuk menghidupi diri. Bapaknya menikah lagi dan tinggal di rumah bersama istri keduanya, sehingga Umi, Asrul, dan Irsal pindah ke rumah peninggalan orang tua Umi. Berpisah dari Bapak. Meski Bapak kadang memberi mereka uang, itu tidaklah cukup. Setiap kali Asrul diberi uang oleh Bapak, Asrul selalu mengintip dompetnya, ada kayu manis yang diselipkan Bapak di sana. Asrul tak punya dompet karena ia tak pernah memegang uang. Bila pun dia punya, akan ia berikan pada Umi. Asrul ingin membuatkan rumah untuk Umi suatu saat kelak. Asrul dan Zenna akhirnya bertemu. Mereka berdua bertekad mengangkat derajat dirinya dan keluarganya ke kehidupan yang lebih baik. Mereka bertemu di kampus. Koran Harian Semangat turut merekatkan hubungan mereka. Hingga kelak mereka menikah dan memiliki rumah. Umi dan Umak mereka bawa tinggal bersama. Kehidupan mereka walau sudah lebih baik, tidak juga mudah. Musibah datang berkali-kali. “Kita pernah melewati yang lebih buruk dari ini,” kata mereka saling menguatkan.', ''),
 (24, 2, 'Anotasi KUHP Nasional', ' Eddy O.S. Hiariej & Topo Santoso', 'Raja Grafindo Persada', '2025', 44, 'kuhp.avif', 'Pada 2 Januari 2026, Kitab Undang-Undang Hukum Pidana Nasional (KUHP Nasional) berlaku secara efektif, atau tiga tahun tahun sejak disahkan pada 2 Januari 2023 yang lalu. KUHP Nasional ini menggantikan KUHP Lama yang merupakan salinan dari Wetboek van Strafrecht (WvS) yang telah berlaku di negeri kita sejak 1918. Dengan berlakunya KUHP Nasional itu secara resmi KUHP berbahasa Indonesia digunakan, setelah sebelumnya kita hanya menggunakan KUHP terjemahan dari WvS. KUHP Nasional ini juga dilengkapi dengan penjelasan, baik penjelasan umum maupun penjelasan pasal per pasal.\r\n\r\nNamun, meskipun KUHP Nasional sudah dilengkapi dengan penjelasan umum dan penjelasan pasal per pasal, ternyata tidak semua hal dapat dituangkan ke dalam penjelasan KUHP Nasional tersebut. Berbagai informasi dan penjelasan/ keterangan masih diperlukan untuk makin memahami KUHP Nasional baik untuk asas-asas/prinsip-prinsip dan ajaran/doktrin yang telah dituangkan menjadi norma di Buku I KUHP Nasional, maupun ketentuan-ketentuan di Buku II KUHP Nasional yang sudah jauh berkembang dan berbeda dari KUHP Lama.\r\n\r\nOleh karena itu, buku Anotasi KUHP Nasional dihadirkan untuk dapat lebih memberikan konteks dan pemahaman lebih akurat pada berbagai pasal KUHP Nasional. Buku ini diharapkan dapat dipergunakan dan dimanfaatkan oleh akademisi hukum, mahasiswa hukum, praktisi hukum serta penegak hukum di seluruh Indonesia. Dalam buku ini, pasal demi pasal KUHP Nasional, penjelasan dari pasal demi pasal tersebut, disusun dan disusul dengan anotasi pasal demi pasalnya.\r\n\r\nBuku ini dapat menjadi bahan penting dalam pendidikan hukum pidana di kampus-kampus hukum ataupun lembaga pendidikan dari institusi penegak hukum (misalnya kepolisian, kejaksaan, KPK, kehakiman, advokat) serta bahan penting bagi sosialisasi KUHP Nasional di seluruh Indonesia.\r\n\r\n\r\nTahun Terbit : Cetakan Pertama, Februari 2025\r\n\r\nPernahkah Anda terpikir betapa menariknya dunia yang terbuka lebar lewat lembaran buku? Membaca bukan hanya kegiatan rutin, tetapi juga petualangan tak terbatas ke dalam imajinasi dan pengetahuan.\r\n\r\nMembaca mengasah pikiran, membuka wawasan, dan memperkaya kosakata. Ini adalah pintu menuju dunia di luar kita yang tak terbatas.\r\n\r\nTetapkan waktu khusus untuk membaca setiap hari. Dari membaca sebelum tidur hingga menyempatkan waktu di pagi hari, kebiasaan membaca dapat dibentuk dengan konsistensi.\r\n\r\nPilih buku sesuai minat dan level literasi. Mulailah dengan buku yang sesuai dengan keinginan dan kemampuan membaca.\r\n\r\nTemukan tempat yang tenang dan nyaman untuk membaca. Lampu yang cukup, kursi yang nyaman, dan sedikit musik pelataran bisa menciptakan pengalaman membaca yang lebih baik.\r\n\r\nBuat catatan atau jurnal tentang buku yang telah Anda baca. Tuliskan pemikiran, kesan, dan pelajaran yang Anda dapatkan.', NULL),
 (25, 4, 'The Dip', 'Seth Godin', 'Bhuana Ilmu Populer', '2022', 51, 'dip.avif', 'Kita menghabiskan sebagian besar waktu untuk menangani hambatan dengan kegigihan. Terkadang, kita menjadi patah semangat dan berpaling pada tulisan-tulisan inspiratif, contohnya milik Vince Lombardi yang berbunyi, \"Orang yang mudah berhenti tidak pernah berhenti\". Itu saran yang buruk. Mereka yang menang juga berhenti. Namun, mereka hanya berhenti di waktu yang tepat.\r\n\r\nKebanyakan orang berhenti, hanya saja dengan cara yang salah. Faktanya, banyak profesi dan pasar mendapatkan keuntungan dari orang-orang yang mudah mengalah ketika mereka beranggapan bahwa orang-orang itu ingin berhenti. Justru banyak bisnis dan organisasi berpegang pada prinsip itu.\r\n\r\nSINOPSIS\r\nKapan kita harus menyerah? Dan kapan kita harus berjuang? Ketika kita memulai sesuatu, baik itu hobi, pekerjaan, atau membangun perusahaan baru, awalnya pasti merasa menyenangkan, tetapi lama-kelamaan kondisinya menjadi semakin sulit dan tidak menyenangkan lagi. The Dip dalam buku ini adalah lembah yang harus dilalui ketika kita ingin mengejar sesuatu, yaitu masa-masa sulit ketika semua progres terasa nyaris tidak ada dan kita meragukan banyak hal. Di antara keraguan yang muncul, selalu ada pertanyaan: “Ini layak untuk dilanjutkan atau tidak?” Buku ini membahas soal kapan harus berhenti atau terus berusaha untuk mengejar apa yang kamu inginkan. Apakah harus menyerah? Atau harus terus berjuang agar bisa melewati the dip dan meraih kesuksesan? Temukan cara-caranya disini agar kamu bisa tetap bisa bertahan dan melewati fase the dip, dan meraih kesuksesan.', NULL),
-(26, 1, 'Belajar Tajwid', 'Sofa Azizah', 'Anak Hebat Indonesia', '2026', 98, 'tajwid.avif', 'Buku Belajar Tajwid ini hadir sebagai panduan awal bagi Anda yang ingin memperbaiki bacaan Al-Qur’an, tetapi belum mampu mengakses kitab-kitab klasik atau belajar langsung dari para ulama. Disusun dari berbagai rujukan otoritatif dan pengalaman langsung penulis dalam mengajar serta talaqqi, buku ini menjadi jembatan yang memudahkan pembaca memahami dasar-dasar tajwid secara praktis dan aplikatif.\r\n', NULL),
+(26, 1, 'Belajar Tajwid', 'Sofa Azizah', 'Anak Hebat Indonesia', '2026', 99, 'tajwid.avif', 'Buku Belajar Tajwid ini hadir sebagai panduan awal bagi Anda yang ingin memperbaiki bacaan Al-Qur’an, tetapi belum mampu mengakses kitab-kitab klasik atau belajar langsung dari para ulama. Disusun dari berbagai rujukan otoritatif dan pengalaman langsung penulis dalam mengajar serta talaqqi, buku ini menjadi jembatan yang memudahkan pembaca memahami dasar-dasar tajwid secara praktis dan aplikatif.\r\n', NULL),
 (27, 3, 'Musim yang Tak Sempat Kita Miliki', 'Rintik Sedu', 'Gramedia Pustaka Utama', '2025', 55, 'ss.avif', 'Rani bekerja sebagai editor di sebuah kantor penerbitan. Dia suka dengan kehidupan dan dunia kecilnya yang tenang di pengujung usia 20-annya, meski terkadang rekan kerjanya suka mengusiknya.\r\n\r\nSuatu hari, proyek besar datang dari perusahaan parfum lokal yang berniat merilis produk baru lewat buku. Rani yang sudah lama bermimpi menerbitkan buku pertamanya, akhirnya mendapatkan kesempatan. Tetapi, proses pengerjaan naskah itu membawa Rani kembali pada seseorang dari masa lalunya. Seseorang yang menjadi alasan pertama Rani ingin menulis.\r\n\r\nApakah Rani berhasil merampungkan naskahnya? Atau dia justru semakin terjerat masa lalunya?', NULL),
-(28, 4, 'Majapahit', 'Herald Van Der Linde', 'Kepustakaan Populer gramedia', '2025', 85, 'majapahit.avif', 'Majapahit adalah kerajaan besar di Nusantara yang kekayaan dan kekuasaannya dibangun dari perpaduan muslihat politik, panen raya padi, dan angkatan laut yang sedemikian perkasa sampai-sampai bahkan orang Portugis, pelaut hebat Eropa, pun terkesan. Namun, tidak banyak bukti fisik yang tersisa dari Majapahit. Sumber sejarah juga terbatas dan akurasinya meragukan.\r\n\r\nDitulis berdasarkan sumber-sumber primer dan naskah-naskah kuno, cerita dalam buku ini mengangkat kisah raja-raja sangat eksentrik, perseteruan berdarah keluarga kerajaan, dan kisah tentang juru tulis istana pemabuk, tetapi memiliki kesadaran jernih untuk menuliskan segala yang dilihatnya.', NULL);
+(28, 4, 'Majapahit', 'Herald Van Der Linde', 'Kepustakaan Populer gramedia', '2025', 84, 'majapahit.avif', 'Majapahit adalah kerajaan besar di Nusantara yang kekayaan dan kekuasaannya dibangun dari perpaduan muslihat politik, panen raya padi, dan angkatan laut yang sedemikian perkasa sampai-sampai bahkan orang Portugis, pelaut hebat Eropa, pun terkesan. Namun, tidak banyak bukti fisik yang tersisa dari Majapahit. Sumber sejarah juga terbatas dan akurasinya meragukan.\r\n\r\nDitulis berdasarkan sumber-sumber primer dan naskah-naskah kuno, cerita dalam buku ini mengangkat kisah raja-raja sangat eksentrik, perseteruan berdarah keluarga kerajaan, dan kisah tentang juru tulis istana pemabuk, tetapi memiliki kesadaran jernih untuk menuliskan segala yang dilihatnya.', NULL),
+(30, 3, 'Pergi', 'Tere Liye', 'Republika', '2018', 22, 'pergis.jpg', 'Sebuah kisah tentang menemukan tujuan, ke mana hendak pergi, melalui kenangan demi kenangan masa lalu, pertarungan hidup-mati, untuk memutuskan ke mana langkah kaki akan dibawa. \r\n\r\nPergi', '');
 
 -- --------------------------------------------------------
 
@@ -111,7 +112,8 @@ INSERT INTO `pesan` (`id_pesan`, `nama`, `email`, `pesan`, `created_at`, `status
 (7, 'Abdul Aziz', 'edikrn577@gmail.com', 'aw', '2026-04-08 03:13:30', 'belum'),
 (8, 'Syahril Gani Akbar', 'syahril77@gmail.com', 'Fitur peminjaman dan pencatatan transaksi sangat membantu pengelolaan data.', '2026-04-11 03:24:09', 'dibaca'),
 (9, 'Pradika Putra', 'dika25@gmail.com', 'Sistemnya memudahkan pencarian buku dan proses peminjaman jadi lebih cepat.', '2026-04-11 03:25:16', 'dibaca'),
-(10, 'Lutfi Jayadi', 'jayadi09@gmail.com', 'Interface sederhana dan mudah digunakan, cocok untuk sistem perpustakaan.', '2026-04-11 03:26:20', 'dibaca');
+(10, 'Lutfi Jayadi', 'jayadi09@gmail.com', 'Interface sederhana dan mudah digunakan, cocok untuk sistem perpustakaan.', '2026-04-11 03:26:20', 'dibaca'),
+(11, 'Siti Rohmah', 'edikrn577@gmail.com', 'as', '2026-04-15 02:47:51', 'belum');
 
 -- --------------------------------------------------------
 
@@ -147,8 +149,9 @@ INSERT INTO `transaksi` (`id_transaksi`, `id_user`, `id_buku`, `jumlah`, `tangga
 (68, 6, 26, 1, '2026-04-11', '2026-04-21', 'dikembalikan', 0),
 (69, 12, 26, 1, '2026-04-11', '2026-04-21', 'dikembalikan', 0),
 (70, 12, 26, 2, '2026-04-11', '2026-04-21', 'dipinjam', 1),
-(71, 15, 26, 1, '2026-04-11', '2026-04-13', 'menunggu konfirmasi', 0),
-(72, 11, 12, 1, '2026-04-11', '2026-04-13', 'dipinjam', 0);
+(71, 15, 26, 1, '2026-04-11', '2026-04-13', 'dikembalikan', 1),
+(72, 11, 12, 1, '2026-04-11', '2026-04-13', 'dipinjam', 1),
+(73, 15, 28, 2, '2026-04-15', '2026-04-18', 'dikembalikan', 1);
 
 -- --------------------------------------------------------
 
@@ -225,7 +228,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT untuk tabel `kategori`
@@ -237,13 +240,13 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT untuk tabel `pesan`
 --
 ALTER TABLE `pesan`
-  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
